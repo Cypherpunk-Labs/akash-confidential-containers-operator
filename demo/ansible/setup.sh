1e47c9115a49a0ac6a30c8dcca147058afa6f579
kempy007 <13536174+kempy007@users.noreply.github.com>
2023-09-12 14:59:22 +0100

    added setup scripts

--- demo/ansible/setup.sh
+++ demo/ansible/setup.sh
@@ -4,3 +4,11 @@ ansible-playbook -i 127.0.0.1 --connection=local install_containerd.yml
 ansible-playbook -i 127.0.0.1 --connection=local install_kubeadm.yml
 ansible-playbook -i 127.0.0.1 --connection=local install_bridge.yml
 sudo ./up.sh
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
---
+++ demo/kata-chk.sh
@@ -0,0 +1,1 @@
+sudo kata-containers.runtime check
