84e470576f869debeed2e214c392b6ca529efdf6
kempy007 <13536174+kempy007@users.noreply.github.com>
2023-09-12 12:16:51 +0100

    fix setup.sh

--- demo/ansible/setup.sh
+++ demo/ansible/setup.sh
@@ -3,4 +3,4 @@ apt -y install ansible
 ansible-playbook -i 127.0.0.1 --connection=local install_containerd.yml
 ansible-playbook -i 127.0.0.1 --connection=local install_kubeadm.yml
 ansible-playbook -i 127.0.0.1 --connection=local install_bridge.yml
-./up.sh
+sudo ./up.sh
